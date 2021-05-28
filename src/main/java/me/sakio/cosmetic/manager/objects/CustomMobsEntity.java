f30a6c7ad2747bff7dc536a01e2d18d35fca6e7b
Sakiio <65667910+Sakiio@users.noreply.github.com>
2021-05-28 22:51:58 +0200

    Commit
    Start with custom mobs

---
+++ src/main/java/me/sakio/cosmetic/manager/objects/CustomMobsEntity.java
@@ -0,0 +1,17 @@
+package me.sakio.cosmetic.manager.objects;
+
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+/**
+ * Project: Cosmetic
+ * Date: 28/05/2021 @ 22:43
+ * Class: CustomMobsEntity
+ */
+public class CustomMobsEntity {
+    public void customPet(Location loc, Player player){
+//        super(EntityType.CREEPER, (()loc.getWorld())
+
+    }
+}
